6c2b006bf18b1bd8f7f692acb56e5d6914881d98
Luis2003-Henrique <luisinhodogas@gmail.com>
2024-09-30 21:54:24 -0300

    carrinho

---
+++ js/app.js
@@ -0,0 +1,75 @@
+let foneDeOuvido = 100;
+let celular = 1400;
+let oculosVr = 5000;
+
+let valorSomado = 0;
+
+let listaDeSomas = [];
+
+let total = document.getElementById('valor-total');
+
+
+adicionar();
+
+function adicionar(){
+
+    let produtos = document.getElementById('produto');
+    let produtoSelecionado = produtos.value;
+    
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    
+    let carrinho = document.getElementById('lista-produtos');
+
+    if (produtoSelecionado == 'Fone de ouvido - R$100'){
+        let valorTotalDoProduto = foneDeOuvido * quantidade;
+        listaDeSomas.push(valorTotalDoProduto);
+        atualizarValorTotal()
+        let novoItem1 = document.createElement('li');
+        carrinho.innerHTML = '';
+        novoItem1.textContent = (`${quantidade}x Fone de ouvido R$${foneDeOuvido}`);
+        carrinho.appendChild(novoItem1);
+    
+    } else if (produtoSelecionado == ('Celular - R$1400')){
+        valorTotalDoProduto = celular * quantidade;
+        listaDeSomas.push(valorTotalDoProduto);
+        atualizarValorTotal()
+        let novoItem2 = document.createElement('li');
+        novoItem2.textContent = (`${quantidade}x Celular R$${celular}`);
+        carrinho.appendChild(novoItem2);
+        
+    } else if (produtoSelecionado == ('Oculus VR - R$5000')){
+        valorTotalDoProduto = oculosVr * quantidade;
+        listaDeSomas.push(valorTotalDoProduto);
+        atualizarValorTotal()
+        let novoItem3 = document.createElement('li');
+        novoItem3.textContent = (`${quantidade}x Óculos VR R$${oculosVr}`);
+        carrinho.appendChild(novoItem3);
+
+}
+}
+
+limpar();
+
+function limpar(){
+    let limparProdutos = document.getElementById('lista-produtos');
+    let limparValor = document.getElementById('valor-total');
+    let limparQuantidade = document.getElementById('quantidade');
+limparProdutos.textContent = '';
+limparValor.textContent = 'R$0';
+limparQuantidade.value = '';
+listaDeSomas = [];
+}
+
+function atualizarValorTotal(){
+    valorSomado = 0;
+    for (i = 0; i < listaDeSomas.length; i++){
+        valorSomado += listaDeSomas[i];
+    }
+    total.textContent = (`R$${valorSomado}`);
+}
+
+
+// Adicionar um botão para remover itens do carrinho: Isso permitiria que o usuário removesse itens que ele não deseja mais comprar.
+// Criar um sistema de desconto: Você poderia implementar um sistema de desconto para incentivar o usuário a comprar mais produtos.
+// Implementar um sistema de frete: Você poderia calcular o frete com base no valor total da compra ou na região de entrega.
+// Criar um sistema de login: Você poderia permitir que o usuário faça login para salvar seus dados de compra e histórico de pedidos.
